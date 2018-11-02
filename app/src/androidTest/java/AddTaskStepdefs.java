56363abde61253aaf5a431842d9b6fb5a376f17c
Angelika <Angelika>
2018-11-02 18:50:09 +0100

    [Front][Feat]Created Step Definitions for Create Mission

--- app/src/androidTest/java/AddTaskStepdefs.java
+++ app/src/androidTest/java/AddTaskStepdefs.java
@@ -1,4 +1,5 @@
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -18,6 +19,132 @@ class AddTaskStepdefs {
 
     @Then("^I should see a new Task$")
     public void iShouldSeeANewTask() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am seeing Missions$")
+    public void iAmSeeingMissions() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I press on Create Task$")
+    public void iPressOnCreateTask() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see the Configuartion of a new Mission$")
+    public void iShouldSeeTheConfiguartionOfANewMission() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am seeing a window to cofigure the new Mission$")
+    public void iAmSeeingAWindowToCofigureTheNewMission() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I enter \"([^\"]*)\" into input field having id \"([^\"]*)\"$")
+    public void iEnterIntoInputFieldHavingId(String arg0, String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I click on button having text \"([^\"]*)\"$")
+    public void iClickOnButtonHavingText(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see a new Mission$")
+    public void iShouldSeeANewMission() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on button havong text \"([^\"]*)\"$")
+    public void iClickOnButtonHavongText(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see a window to add Missionmembers$")
+    public void iShouldSeeAWindowToAddMissionmembers() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am seeing a window to add Missionmembers$")
+    public void iAmSeeingAWindowToAddMissionmembers() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I enter \"([^\"]*)\" into search field having id \"([^\"]*)\"$")
+    public void iEnterIntoSearchFieldHavingId(String arg0, String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Missionmember \"([^\"]*)\" appears in List$")
+    public void missionmemberAppearsInList(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am seeing a window to add Missionmembers with Missionmember in the List$")
+    public void iAmSeeingAWindowToAddMissionmembersWithMissionmemberInTheList() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on the Missionmember \"([^\"]*)\"$")
+    public void iClickOnTheMissionmember(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the Missionmember \"([^\"]*)\" is selected$")
+    public void theMissionmemberIsSelected(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am seeing a window to add Missionmembers with a selected Missionmember in the List$")
+    public void iAmSeeingAWindowToAddMissionmembersWithASelectedMissionmemberInTheList() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on the selected Missionmember \"([^\"]*)\"$")
+    public void iClickOnTheSelectedMissionmember(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the Missionmember \"([^\"]*)\" is not selected$")
+    public void theMissionmemberIsNotSelected(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^I am seeing a window to add Missionmembers with selcted Missionmembers in the List$")
+    public void iAmSeeingAWindowToAddMissionmembersWithSelctedMissionmembersInTheList() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on Add Members$")
+    public void iClickOnAddMembers() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should return to the Configuration of the Mission$")
+    public void iShouldReturnToTheConfigurationOfTheMission() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
