7fae547f218193eddfdeb5c4b543e24351eb84ad
JoHi21 <38754316+JoHi21@users.noreply.github.com>
2018-11-09 17:27:02 +0100

    [Front][Feat] Set up Login Screen

--- app/src/main/java/learnityourself/dhbw/learnityourself/MainActivity.java
+++ app/src/main/java/learnityourself/dhbw/learnityourself/MainActivity.java
@@ -2,6 +2,10 @@ package learnityourself.dhbw.learnityourself;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText username_field = findViewById(R.id.username_field);
+        final EditText password_field = findViewById(R.id.password_field);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener(){
+           public void onClick(View v){
+               Toast.makeText(MainActivity.this, password_field.getText(), Toast.LENGTH_SHORT).show();
+           }
+        });
     }
+
+
+
 }
