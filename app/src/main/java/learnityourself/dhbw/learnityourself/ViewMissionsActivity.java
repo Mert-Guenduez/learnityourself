384120a7b8217f66abcf513e077cb6bb179f62f9
JoHi21 <38754316+JoHi21@users.noreply.github.com>
2018-11-14 16:48:36 +0100

    [Front][Bug] Fixed authorization

--- app/src/main/java/learnityourself/dhbw/learnityourself/ViewMissionsActivity.java
+++ app/src/main/java/learnityourself/dhbw/learnityourself/ViewMissionsActivity.java
@@ -3,7 +3,10 @@ package learnityourself.dhbw.learnityourself;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -14,11 +17,25 @@ import learnityourself.dhbw.learnityourself.utility.HTTPRequestHandler;
 public class ViewMissionsActivity extends AuthorizedActivity {
 
     List<Mission> missions;
+
+    Button newMissionButton;
     User user;
     @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_missions);
+        if(checkAuthorized())init();
+    }
+
+    @Override
+    void init() {
+        newMissionButton = findViewById(R.id.newMission_button);
+        newMissionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ViewMissionsActivity.this, "Not yet implemented.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
