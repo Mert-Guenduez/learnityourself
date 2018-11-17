ef4e4df75edc94de27078aea2060340c1cf75702
Angelika <Angelika>
2018-11-17 20:43:57 +0100

    [Front][Feat] Updated viewMission, Created viewMissionInformation and menu

--- app/src/main/java/learnityourself/dhbw/learnityourself/ViewMissionActivity.java
+++ app/src/main/java/learnityourself/dhbw/learnityourself/ViewMissionActivity.java
@@ -1,7 +1,11 @@
 package learnityourself.dhbw.learnityourself;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import java.util.List;
 
@@ -9,6 +13,7 @@ import learnityourself.dhbw.learnityourself.model.Mission;
 import learnityourself.dhbw.learnityourself.model.User;
 
 public class ViewMissionActivity extends AppCompatActivity {
+
     Mission mission;
     User user;
 
@@ -17,6 +22,31 @@ public class ViewMissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_mission);
+
+        final int abTitleId = getResources().getIdentifier("action_bar_title", "id", "android");
+        findViewById(abTitleId).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewMissionActivity.this, ViewMissionInformationActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.add){
+            // add create new Task
+        }
+        return true;
     }
 
 }
