ddf767a81381e3324e457255745a5c8871b1303e
JoHi21 <38754316+JoHi21@users.noreply.github.com>
2018-11-28 10:58:45 +0100

    [Front][Bug] changed tests

--- app/src/androidTest/assets/res/Login.feature
+++ app/src/androidTest/assets/res/Login.feature
@@ -6,13 +6,13 @@ Feature: Log into app
     And I enter "12345" into the password field
     And I click on the Login Button
     Then I should see the Main Activity
-#
-#   Scenario: Wrong Password
-#   Given I am seeing input fields for login
-#   When I enter "testuser1" into the username field
-#   And I enter "13" into the password field
-#   And I click on the Login Button
-#   Then I should see the error message Wrong password or username
+
+   #Scenario: Wrong Password
+   #Given I am seeing input fields for login
+   #When I enter "testuser1" into the username field
+   #And I enter "13" into the password field
+   #And I click on the Login Button
+   #Then I should see the error message Wrong password or username
 #
 # Scenario: Wrong Username
 #   Given I am seeing input fields for login
--- app/src/androidTest/java/learnityourself/dhbw/learnityourself/test/LoginStepdefs.java
+++ app/src/androidTest/java/learnityourself/dhbw/learnityourself/test/LoginStepdefs.java
@@ -1,6 +1,7 @@
 package learnityourself.dhbw.learnityourself.test;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -8,6 +9,8 @@ import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -19,6 +22,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
@@ -26,17 +31,16 @@ public class LoginStepdefs {
 
 
     @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class, false, false);
 
-    private Activity activity = activityTestRule.getActivity();
+    private static Activity activity;// = activityTestRule.getActivity();
 
 
     @Given("^I am seeing input fields for login$")
     public void iAmSeeingInputFieldsForLogin() throws Throwable {
         activityTestRule.launchActivity(new Intent());
         activity = activityTestRule.getActivity();
-     onView(withId(R.id.LoginActivity));
-
+        onView(withId(R.id.LoginActivity));
     }
 
     @When("^I enter \"([^\"]*)\" into the username field$")
@@ -53,17 +57,18 @@ public class LoginStepdefs {
 
     @And("^I click on the Login Button$")
     public void iClickOnTheLoginButton() throws Throwable {
-        onView(withId(R.id.username_field))
+        onView(withId(R.id.login_button))
                 .perform(click());
     }
 
     @Then("^I should see the Main Activity$")
     public void iShouldSeeTheMainActivity() throws Throwable {
-        onView(withId(R.id.MainActivity));
+        onView(withId(R.id.MainActivity)).check(matches(isDisplayed()));
     }
 
     @Then("^I should see the error message Wrong password or username$")
     public void iShouldSeeTheErrorMessageWrongPasswordOrUsername() throws Throwable {
 
     }
+
 }
