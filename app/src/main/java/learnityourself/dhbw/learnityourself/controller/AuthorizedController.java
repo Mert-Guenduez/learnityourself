057fd3b9dd1cd208ebe40f633dd38543dfe4d38a
JoHi21 <38754316+JoHi21@users.noreply.github.com>
2018-12-02 15:42:05 +0100

    [Front][Bug] Extracted Logik from ViewMissionsActivity

---
+++ app/src/main/java/learnityourself/dhbw/learnityourself/controller/AuthorizedController.java
@@ -0,0 +1,61 @@
+package learnityourself.dhbw.learnityourself.controller;
+
+import android.content.Context;
+import android.content.Intent;
+
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
+import learnityourself.dhbw.learnityourself.LoginActivity;
+import learnityourself.dhbw.learnityourself.model.User;
+import learnityourself.dhbw.learnityourself.utility.HTTPRequestHandler;
+
+public abstract class AuthorizedController {
+
+    protected User user;
+    protected Context context;
+
+    public AuthorizedController(User user, Context context){
+
+        this.user = user;
+        this.context = context;
+
+        if(checkAuthorized()){
+            init();
+        }
+    }
+
+    protected abstract void init();
+
+    private boolean checkAuthorized(){
+
+        if(user==null){
+            context.startActivity(new Intent(context, LoginActivity.class));
+            return false;
+        }
+
+        HTTPRequestHandler handler = new HTTPRequestHandler();
+        InputStream in  = null;
+        try {
+            in = handler.execute("https://91.205.172.109/login.php","username", user.getUsername(),"sessionkey",user.getSessionkey()).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //   Toast.makeText(MainActivity.this, getStringFromInputStream(in), Toast.LENGTH_SHORT).show();
+        String inputString=HTTPRequestHandler.getStringFromInputStream(in);
+
+        if(inputString.contains("true")) {
+            return true;
+        }else{
+            context.startActivity(new Intent(context, LoginActivity.class));
+            return false;
+        }
+
+    }
+
+    protected  User getUser(){
+        return user;
+    }
+}
---
+++ app/src/main/java/learnityourself/dhbw/learnityourself/controller/ViewMissionsController.java
@@ -0,0 +1,52 @@
+package learnityourself.dhbw.learnityourself.controller;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
+import learnityourself.dhbw.learnityourself.ViewMissionActivity;
+import learnityourself.dhbw.learnityourself.ViewMissionsActivity;
+import learnityourself.dhbw.learnityourself.model.Mission;
+import learnityourself.dhbw.learnityourself.model.User;
+import learnityourself.dhbw.learnityourself.utility.HTTPRequestHandler;
+
+public class ViewMissionsController extends AuthorizedController{
+
+    Mission[] missions;
+    public ViewMissionsController(User user, Context context){
+        super(user, context);
+    }
+    @Override
+    protected void init() {
+        HTTPRequestHandler handler = new HTTPRequestHandler();
+        InputStream in  = null;
+        try {
+            in = handler.execute("https://91.205.172.109/allMissionsFromUser.php","username", user.getUsername(),"sessionkey", user.getSessionkey()).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd' 'HH:mm:ss")
+                .create();
+        missions = gson.fromJson(HTTPRequestHandler.getStringFromInputStream(in), Mission[].class);
+    }
+
+    public Mission[] getMissions(){
+        return missions;
+    }
+
+    public void missionClicked(int position) {
+        Intent intent = new Intent(context, ViewMissionActivity.class);
+        intent.putExtra("user", user);
+        intent.putExtra("mission", missions[position]);
+        context.startActivity(intent);
+    }
+}
