9cb79926662dca3267390d9d87eaf6550e0946d3
Mert-Guenduez <42411990+Mert-Guenduez@users.noreply.github.com>
2019-04-29 20:08:25 +0200

    [URGENT][Back][Test] Added Login php test

--- Backend/tests/LoginTestPHPUnit.php
+++ Backend/tests/LoginTestPHPUnit.php
@@ -8,12 +8,12 @@ class AllUsersFromMissinPHPUnit extends Testcase{
         public function testLogin(){
         // Bei Angabe korrekter Daten unter Verwendung des Passwort muss 'authentication' => 'true' sein und ein Sessionkey zurückgegeben werden
 				
-				$_POST['username'] = 'testuser1';
+	$_POST['username'] = 'testuser1';
         $_POST['password'] = '12345';
         $_POST['sessionkey'] = NULL;
-				require('../login.php');
+	require('../login.php');
         $this->assertTrue($data["authentication"] === 'true');
-				$this->assertArrayHasKey("sessionkey", $data);
+	$this->assertArrayHasKey("sessionkey", $data);
 
         // Bei Angabe korrekter Daten unter Verwendung des Sessionkeys muss 'authentication' => 'true' sein
         $_POST['username'] = 'testuser1';
@@ -29,21 +29,21 @@ class AllUsersFromMissinPHPUnit extends Testcase{
         require('../login.php');
         $this->assertTrue($data["error"] === 'secret not set');
 				
-				// Bei Auslassung des Username muss 'error' => 'username not set' sein
-				$_POST['username'] = NULL;
+	// Bei Auslassung des Username muss 'error' => 'username not set' sein
+	$_POST['username'] = NULL;
         $_POST['password'] = '12345';
         $_POST['sessionkey'] = NULL;
         require('../login.php');
         $this->assertTrue($data["error"] === 'username not set');
 				
-				// Falls entweder der Username oder das Passwort (oder beides) nicht stimmt, muss 'error' => 'Authentication failure' sein
-				$_POST['username'] = 'testuser1';
+	// Falls entweder der Username oder das Passwort (oder beides) nicht stimmt, muss 'error' => 'Authentication failure' sein
+	$_POST['username'] = 'testuser1';
         $_POST['password'] = '54321';
         $_POST['sessionkey'] = NULL;
         require('../login.php');
         $this->assertTrue($data["error"] === 'Authentication failure');
 				
-				$_POST['username'] = 'unregisteredUser';
+	$_POST['username'] = 'unregisteredUser';
         $_POST['password'] = '12345';
         $_POST['sessionkey'] = NULL;
         require('../login.php');
