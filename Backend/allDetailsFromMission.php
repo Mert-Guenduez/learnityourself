97e8c2e985645d3b9e844e811cfa304e3c344c06
Mert <mert.gunduz1997@gmail.com>
2019-05-08 20:09:27 +0200

    [Back][Feat] Modified allDetailsFromMission as requested

--- Backend/allDetailsFromMission.php
+++ Backend/allDetailsFromMission.php
@@ -8,8 +8,7 @@ $missionid  = $_POST['missionid'];
 
 $db = new mysqli('localhost', 'XXX', 'XXX', 'XXX');
 if ($db->connect_errno > 0) {
-    die(json_encode(array('authentication' => 'false', 'error' => 'Unable to connect to database [' . $db->connect_error . ']')));
-}
+    die(json_encode(array('authentication' => 'false', 'error' => 'Unable to connect to database [' . $db->connect_erro$}
 if (! isset($_POST['missionid'])) {
     die(json_encode(array('error' => 'mission identification not set')));
 }
@@ -28,13 +27,13 @@ if($check) {
     $data = array('missionid' => $missionid, 'owner' => $owner, 'missionname' => $missionname, 'description' => $description, 'deadline' => $deadline, 'tasks' => array());
     $stmt->close();
 
-    $stmt = $db->prepare('SELECT id,name FROM tasks WHERE missionid = ?');
-    $stmt->bind_param('i', $missionid);
+    $stmt = $db->prepare('SELECT tasks.id,tasks.name, userToTask.completed FROM tasks INNER JOIN userToTask ON tasks.id = userToTask.taskid WHERE tasks.missionid = ? AND userToTask.username = ?');
+    $stmt->bind_param('is', $missionid, $username);
     $stmt->execute();
-    $stmt->bind_result($taskid, $taskname);
+    $stmt->bind_result($taskid, $taskname, $completed);
     while ($stmt->fetch()) {
         $taskname = utf8_encode($taskname);
-        $taskhead = array('taskid' => $taskid, 'taskname' => $taskname);
+        $taskhead = array('taskid' => $taskid, 'taskname' => $taskname, 'completed' => $completed);
         $data['tasks'][] = $taskhead;
     }
     $stmt->close();
