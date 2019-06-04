6a4ca77f6557707014dfbcf428fd08e904c0beac
JannikAdam97 <jannikadam1@web.de>
2019-06-04 16:17:40 +0200

    [Back][Misc] Refactoring register

--- Backend/register.php
+++ Backend/register.php
@@ -1,40 +1,67 @@
 <?php
 header('Content-Type: application/json;charset=utf-8');
-$mysqli = new mysqli("XXX", "XXX", "XXX", "XXX"); //censored credentials
-if ($mysqli->connect_errno > 0) {
-    die('Unable to connect to database [' . $mysqli->connect_error . ']');
-}
-$username = $_POST["username"];
-$pw       = $_POST["password"];
-//prevent sql injection
-$stmt = $mysqli->prepare("SELECT * FROM users WHERE user_name = ?");
-$stmt->bind_param('s', $username);
 
-$stmt->execute();
-$result = $stmt->get_result();
-$row = mysqli_fetch_row($result);
-$result = $mysqli->query("SELECT COUNT(*) FROM users");
-$rowcount = mysqli_fetch_row($result);
-if(count($row)>0){
-        $data = array('response' => 'User already exists');
-}
-elseif($rowcount[0] >=1000){
-        $data = array('response' => 'DB limit of 1000 Users reached');
-}
-else{
-        $salt_hash = random_bytes(255);
-        $hashedpw = hash_pbkdf2("sha256",$pw,$salt_hash, 1000);
-        $stmt = $mysqli->prepare("INSERT INTO users (user_name, user_password, salt_hash)
-        VALUES (?,?,?)");
-        $stmt->bind_param('sss',$username,$hashedpw,$salt_hash);
+$params = array(
+        'username',
+        'password'
+    );
+    
+    // returns $paramsCheck - parameters all set true/false
+    // returns $data - failure message
+    include 'checkParameters.php';
+    
+    if ($paramsCheck) {
+        $username = $_POST[$params[0]];
+        $password = $_POST[$params[1]];
+    
+        // returns $db - database connection
+        // returns $dbCheck - database connection status
+        // returns $data - database connection failure message
+        include 'connectToDatabase.php';
+    
+        if ($dbCheck) {
 
-        if($stmt->execute()){
-        $data = array('response' => 'User successfully created');
-        }
-        else{
-        $data = array('response' => 'Error while trying to insert into DB');
+                // Check if user is already registered
+                $stmt = $db->prepare('SELECT COUNT(*)
+                                        FROM users
+                                        WHERE user_name = ?');
+                $stmt->bind_param('s', $username);
+                $stmt->execute();
+                $stmt->bind_result($userExistsCount);
+                $stmt->fetch();
+
+                if ($userExistsCount == 1) {
+                        echo json_encode(array('response' => 'User already exists'));
+                        return;
+                }
+                $stmt->close();
+
+                // Check if user count is under 1000
+                $stmt = $db->prepare('SELECT COUNT(*)
+                                        FROM users');
+                $stmt->execute();
+                $stmt->bind_result($usersCount);
+                $stmt->fetch();
+
+                if ($usersCount >= 1000) {
+                        echo json_encode(array('response' => 'DB limit of 1000 Users reached'));
+                        return;
+                }
+                $stmt->close();
+
+                $salt_hash = random_bytes(255);
+                $hashedpw = hash_pbkdf2("sha256", $password, $salt_hash, 1000);
+                $stmt = $db->prepare('INSERT
+                                        INTO users (user_name, user_password, salt_hash)
+                                        VALUES (?,?,?)');
+                $stmt->bind_param('sss', $username, $hashedpw, $salt_hash);
+
+                if($stmt->execute()){
+                        $data = array('response' => 'User successfully created');
+                } else {
+                        $data = array('response' => 'Error while trying to insert into DB');
+                }
         }
 }
-        $json = json_encode($data);
-        echo $json;
+echo json_encode($data);
 ?>
