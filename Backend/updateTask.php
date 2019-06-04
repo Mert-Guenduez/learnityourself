98ae4428274a49ad15c89f49c8426b4b619b82a7
JannikAdam97 <jannikadam1@web.de>
2019-06-04 16:43:28 +0200

    [Back][Misc] Refactoring updateTask

--- Backend/updateTask.php
+++ Backend/updateTask.php
@@ -1,57 +1,97 @@
 <?php
-//ini_set('display_errors', 1);
 header('Content-Type: application/json; charset=utf8');
-$username   = $_POST['username'];
-$sessionkey = $_POST['sessionkey'];
-$missionid  = $_POST['missionid'];
-$taskname  = $_POST['taskname'];
-$taskid  = $_POST['taskid'];
-$description    = $_POST['description'];
-$effort   = $_POST['effort'];
-$db = new mysqli('localhost', 'XXX', 'XXX', 'XXX');
-if ($db->connect_errno > 0) {
-    die(json_encode(array('authentication' => 'false', 'error' => 'Unable to connect to database [' . $db->connect_error . ']')));
-}
-if (! isset($_POST['missionid'])) {
-    die(json_encode(array('error' => 'mission id not set')));
-}
-if (! isset($_POST['taskname'])) {
-    die(json_encode(array('error' => 'taskname not set')));
-}
-include 'checkSessionkey.php';
-$json = json_encode(array('authentication' => 'false', 'error' => 'Authentication failure'));
-if($check) {
-	//Check mission
-    $stmt = $db->prepare('SELECT COUNT(*) FROM missions WHERE id = ?');
-    $stmt->bind_param('i',$missionid);
-    $stmt->execute();
-    $stmt->bind_result($count);
-    $stmt->fetch();
-    if ($count == 0) {
-        die(json_encode(array('error' => 'mission does not exist')));
+
+$params = array(
+    'username',
+    'sessionkey',
+    'missionid',
+    'taskname',
+    'taskid',
+    'description',
+    'effort'
+);
+
+// returns $paramsCheck - parameters all set true/false
+// returns $data - failure message
+include 'checkParameters.php';
+
+if ($paramsCheck) {
+    $username    = $_POST[$params[0]];
+    $sessionkey  = $_POST[$params[1]];
+    $missionid   = $_POST[$params[2]];
+    $taskname    = $_POST[$params[3]];
+    $taskid      = $_POST[$params[4]];
+    $description = $_POST[$params[5]];
+    $effort      = $_POST[$params[6]];
+
+    // returns $db - database connection
+    // returns $dbCheck - database connection status
+    // returns $data - database connection failure message
+    include 'connectToDatabase.php';
+
+    if ($dbCheck) {
+
+        // returns $check - authentication status
+        // returns $data - authentication failure message
+        include 'checkSessionkey.php';
+
+        if($check) {
+            //Check mission
+            $stmt = $db->prepare('SELECT COUNT(*)
+                                    FROM missions
+                                    WHERE id = ?');
+            $stmt->bind_param('i', $missionid);
+            $stmt->execute();
+            $stmt->bind_result($missionCount);
+            $stmt->fetch();
+            if ($missionCount == 0) {
+                echo json_encode(array('error' => 'mission does not exist'));
+                return;
+            }
+            $stmt->close();
+
+            //Check if user is in the mission
+            $stmt = $db->prepare('SELECT COUNT(*)
+                                    FROM userToMission
+                                    WHERE missionid = ? AND username = ?');
+            $stmt->bind_param('is', $missionid, $username);
+            $stmt->execute();
+            $stmt->bind_result($userCount);
+            $stmt->fetch();
+            if ($userCount == 0) {
+                echo json_encode(array('error' => 'The user is not in this mission'));
+                return;
+            }
+            $stmt->close();
+
+            //Check if task is part of the mission
+            $stmt = $db->prepare('SELECT COUNT(*)
+                                    FROM tasks
+                                    WHERE missionid = ? AND id = ?');
+            $stmt->bind_param('ii', $missionid, $taskid);
+            $stmt->execute();
+            $stmt->bind_result($taskInMissionCount);
+            $stmt->fetch();
+            if ($taskInMissionCount == 0) {
+                echo json_encode(array('error' => 'The task is not part of the mission.'));
+                return;
+            }
+            $stmt->close();
+
+            //Update Task
+            $stmt = $db->prepare('UPDATE tasks
+                                    SET name = ?, description = ?, effort = ?
+                                    WHERE id = ?');
+            $stmt->bind_param('sssi', $taskname, $description, $effort, $taskid);
+            if($stmt->execute()) {
+                $data = json_encode(array('response' => 'Task successfully updated'));
+            }
+            else{
+                $data = json_encode(array('error' => 'Task could not be inserted into DBTable tasks'));
+            }
+            $stmt->close();
+        }
     }
-    $stmt->close();
-	//Check if user is in the mission
-	$stmt = $db->prepare('SELECT COUNT(*) FROM userToMission WHERE missionid = ? AND username = ?');
-    $stmt->bind_param('ss',$missionid, $username);
-    $stmt->execute();
-    $stmt->bind_result($count);
-    $stmt->fetch();
-    if ($count == 0) {
-        die(json_encode(array('error' => 'The user is not in this mission')));
-    }
-    $stmt->close();
-	//Update Task
-    $stmt = $db->prepare('UPDATE tasks SET name = ?, description = ?, effort = ? WHERE id = ?');
-    $stmt->bind_param('ssss',$taskname,$description,$effort,$taskid);
-    if($stmt->execute()){
-		$stmt->close();
-		$data = json_encode(array('error' => 'Task successfully updated'));
-	}
-	else{
-		$data = json_encode(array('error' => 'Task could not be inserted into DBTable tasks'));
-	}
-    $json = json_encode($data);
 }
-echo $json;
+echo json_encode($data);
 ?>
