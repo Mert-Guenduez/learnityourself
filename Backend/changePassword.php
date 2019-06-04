09028a5be0673e2984392fdacd31696bfc42cd9a
JannikAdam97 <jannikadam1@web.de>
2019-06-04 12:18:28 +0200

    [Back][Misc] Refactoring changePassword

--- Backend/changePassword.php
+++ Backend/changePassword.php
@@ -1,61 +1,69 @@
 <?php
 header('Content-Type: application/json;charset=utf-8');
 
-$mysqli = new mysqli("localhost", "dblover", "DDBPw1mnmk1337", "learnityourself");
+$params = array(
+    'username',
+    'sessionkey',
+    'password',
+    'passwordNew'
+);
 
-if ($mysqli->connect_errno > 0) {
-    die('Unable to connect to database [' . $mysqli->connect_error . ']');
-}
+// returns $paramsCheck - parameters all set true/false
+// returns $data - failure message
+include 'checkParameters.php';
 
-$username = $_POST["username"];
-$password = $_POST["password"];
-$sessionkey = $_POST["sessionkey"];
-$password_new = $_POST["passwordNew"];
+if ($paramsCheck) {
+    $username    = $_POST[$params[0]];
+    $sessionkey  = $_POST[$params[1]];
+    $password    = $_POST[$params[2]];
+    $password_new = $_POST[$params[3]];
 
-if (! isset($_POST["password"])){
-    $data = array('error' => 'Secret not set.');
-    echo json_encode($data);
-    return;
-}
+    // returns $db - database connection
+    // returns $dbCheck - database connection status
+    // returns $data - database connection failure message
+    include 'connectToDatabase.php';
 
-if (! isset($_POST["passwordNew"])){
-    $data = array('error' => 'New secret not set.');
-    echo json_encode($data);
-    return;
-}
+    if ($dbCheck) {
 
-// returns $check = true/false
-include 'checkSessionkey.php';
+        // returns $check - authentication status
+        // returns $data - authentication failure message
+        include 'checkSessionkey.php';
 
-$data = array('authentication' => 'false', 'error' => 'Authentication failure');
-if($check){
+        if($check) {
 
-    // Check if old password match hashed password in database
-    $stmt = $db->prepare('SELECT user_password,salt_hash FROM users WHERE user_name = ?');
-    $stmt->bind_param('s', $username);
-    $stmt->execute();
-    $stmt->bind_result($password_db,$salt_hash_db);
-    $stmt->store_result();
-    $stmt->fetch();
-    $hashedOldPassword = hash_pbkdf2('sha256',$password,$salt_hash_db, 1000);
-    if($hashedOldPassword == $password_db){
+            // Check if old password match hashed password in database
+            $stmt = $db->prepare('SELECT user_password, salt_hash
+                                    FROM users
+                                    WHERE user_name = ?');
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $stmt->bind_result($password_db, $salt_hash_db);
+            $stmt->store_result();
+            $stmt->fetch();
 
-        // Generate new hashed password
-        $salt_hash = random_bytes(255);
-        $hashedNewPassword = hash_pbkdf2("sha256",$password_new, $salt_hash, 1000);
+            $hashedOldPassword = hash_pbkdf2('sha256', $password, $salt_hash_db, 1000);
+            $data = array('error' => 'Password mismatch.');
+            if($hashedOldPassword == $password_db){
 
-        // Update database
-        $stmt = $db->prepare('UPDATE users SET user_password = ?, salt_hash = ? WHERE user_name = ?');
-        $stmt->bind_param('sss',$hashedNewPassword,$salt_hash,$username);
-        if ($stmt->execute()) {
-            $data = array('success' => 'Password updated.');
-        } else {
-            $data = array('error' => 'Changing password failed.');
+                // Generate new hashed password
+                $salt_hash = random_bytes(255);
+                $hashedNewPassword = hash_pbkdf2("sha256",$password_new, $salt_hash, 1000);
+
+                // Update database
+                $stmt = $db->prepare('UPDATE users
+                                        SET user_password = ?, salt_hash = ?
+                                        WHERE user_name = ?');
+                $stmt->bind_param('sss', $hashedNewPassword, $salt_hash, $username);
+                if ($stmt->execute()) {
+                    $data = array('success' => 'Password updated.');
+                } else {
+                    $data = array('error' => 'Changing password failed.');
+                }
+            }
+            $stmt->free_result();
+            $stmt->close();
         }
     }
-    $stmt->free_result();
-    $stmt->close();
 }
-$json = json_encode($data);
-echo $json;
+echo json_encode($data);
 ?>
