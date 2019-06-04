1ec9a38b08017012dcfc8c53a8face995ab288b3
JannikAdam97 <jannikadam1@web.de>
2019-06-04 15:24:38 +0200

    [Back][Misc] Refactoring logout

--- Backend/logout.php
+++ Backend/logout.php
@@ -1,27 +1,42 @@
 <?php
 header('Content-Type: application/json;charset=utf-8');
 
-$username   = $_POST['username'];
-$sessionkey = $_POST['sessionkey'];
+$params = array(
+    'username',
+    'sessionkey',
+);
 
-$db = new mysqli('localhost', 'XXX', 'XXX', 'XXX');
-if ($db->connect_errno > 0) {
-    die(json_encode(array('authentication' => 'false', 'error' => 'Unable to connect to database [' . $db->connect_error . ']')));
-}
-if (! isset($_POST['username'])) {
-    die(json_encode(array('authentication' => 'false', 'error' => 'username not set')));
-}
+// returns $paramsCheck - parameters all set true/false
+// returns $data - failure message
+include 'checkParameters.php';
 
-include 'checkSessionkey.php';
-$json = json_encode(array('authentication' => 'false', 'error' => 'Authentication failure'));
+if ($paramsCheck) {
+    $username   = $_POST[$params[0]];
+    $sessionkey = $_POST[$params[1]];
 
-if ($check) {
-    $stmt = $db->prepare("UPDATE users SET session_timeout=? WHERE user_name=?");
-    $time_now = date('Y-m-d H:i:s', time());
-    $stmt->bind_param('ss', $time_now, $username);
-    $stmt->execute();
-    $stmt->close();
-    $json = json_encode(array('response' => 'successful'));
+    // returns $db - database connection
+    // returns $dbCheck - database connection status
+    // returns $data - database connection failure message
+    include 'connectToDatabase.php';
+
+    if ($dbCheck) {
+
+        // returns $check - authentication status
+        // returns $data - authentication failure message
+        include 'checkSessionkey.php';
+
+        if($check) {
+            // invalidate sessionkey by setting timeout to now()
+            $stmt = $db->prepare('UPDATE users
+                                    SET session_timeout = ?
+                                    WHERE user_name = ?');
+            $time_now = date('Y-m-d H:i:s', time());
+            $stmt->bind_param('ss', $time_now, $username);
+            $stmt->execute();
+            $stmt->close();
+            $data = array('response' => 'successful');
+        }
+    }
 }
-echo $json;
+echo json_encode($data);
 ?>
