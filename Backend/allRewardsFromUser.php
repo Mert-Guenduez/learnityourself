8e052511e8f84576b4ef9c295bc72697d35e4d01
JannikAdam97 <jannikadam1@web.de>
2019-06-04 11:32:18 +0200

    [Back][Misc] Refactoring allRewardsFromUser

--- Backend/allRewardsFromUser.php
+++ Backend/allRewardsFromUser.php
@@ -1,27 +1,52 @@
 <?php
-//ini_set('display_errors', 1);
 header('Content-Type: application/json; charset=utf8');
-$username   = $_POST['username'];
-$sessionkey = $_POST['sessionkey'];
-$db = new mysqli('localhost', 'XXX','XXX', 'XXX');
-if ($db->connect_errno > 0) {
-    die(json_encode(array('authentication' => 'false', 'error' => 'Unable to connect to database [' . $db->connect_error . ']')));
+
+$params = array(
+    'username',
+    'sessionkey'
+);
+
+// returns $paramsCheck - parameters all set true/false
+// returns $data - failure message
+include 'checkParameters.php';
+
+if ($paramsCheck) {
+    $username   = $_POST[$params[0]];
+    $sessionkey = $_POST[$params[1]];
+
+    // returns $db - database connection
+    // returns $dbCheck - database connection status
+    // returns $data - database connection failure message
+    include 'connectToDatabase.php';
+
+    if ($dbCheck) {
+
+        // returns $check - authentication status
+        // returns $data - authentication failure message
+        include 'checkSessionkey.php';
+
+        if($check) {
+
+            $stmt = $db->prepare('SELECT rewardid, title, description, cost
+                                    FROM rewards
+                                    WHERE owner = ?');
+            $stmt->bind_param('s', $username);
+            $stmt->execute();
+            $allRewards = $stmt->get_result();
+            $stmt->free_result();
+            $stmt->close();
+
+            $data = array();
+            while($row = mysqli_fetch_array($allRewards)){
+                $resultArray = array(
+                    'rewardid' => $row[0],
+                    'title' => $row[1],
+                    'description' => $row[2],
+                    'cost' => $row[3]);
+                $data[] = $resultArray;
+            }
+        }
+    }
 }
-include 'checkSessionkey.php';
-$json = json_encode(array('authentication' => 'false', 'error' => 'Authentication failure'));
-if($check) {
-$stmt = $db->prepare('SELECT rewardid, title, description, cost FROM rewards WHERE owner = ?');
-    $stmt->bind_param('s', $username);
-    $stmt->execute();
-    $allRewards = $stmt->get_result();
-    $stmt->free_result();
-    $stmt->close();
-    $data = array();
-    while($row = mysqli_fetch_array($allRewards)){
-    $resultArray = array('rewardid' => $row[0], 'title' => $row[1], 'description' => $row[2], 'cost' => $row[3]);
-    $data[] = $resultArray;
-}
-    $json = json_encode($data);
-}
-echo $json;
+echo json_encode($data);
 ?>
