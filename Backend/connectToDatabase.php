a385d75c9a50eebbb9e2c294140ab6dfea80635f
JannikAdam97 <jannikadam1@web.de>
2019-06-04 10:44:36 +0200

    [Back][Misc] Refactoring connectToDatabase and checkSessionkey

--- Backend/connectToDatabase.php
+++ Backend/connectToDatabase.php
@@ -1,7 +1,6 @@
 <?php
 $dbCheck = true;
 $db = new mysqli('localhost', 'DB_USER', 'DB_PWD', 'DB_NAME');
-$data = array('db_authentication' => 'true');
 if ($db->connect_errno > 0) {
     $dbCheck = false;
     $data = array('db_authentication' => 'false', 'error' => 'Unable to connect to database [' . $db->connect_error . ']');
