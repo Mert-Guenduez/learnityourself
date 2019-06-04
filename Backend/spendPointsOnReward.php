66943057117420f91b560e66652c47aff0771bef
JannikAdam97 <jannikadam1@web.de>
2019-06-04 16:25:30 +0200

    [Back][Misc] Refactoring spendPointsOnReward

--- Backend/spendPointsOnReward.php
+++ Backend/spendPointsOnReward.php
@@ -2,57 +2,70 @@
 // ini_set('display_errors', 1);
 header('Content-Type: application/json; charset=utf8');
 
-$username   = $_POST['username'];
-$sessionkey = $_POST['sessionkey'];
-$rewardid  = $_POST['rewardid'];
+$params = array(
+    'username',
+    'sessionkey',
+    'rewardid'
+);
 
-// $db - database connection
-// $dbCheck - database connection status
-// $data - database connection failure message
-include 'connectToDatabase.php';
+// returns $paramsCheck - parameters all set true/false
+// returns $data - failure message
+include 'checkParameters.php';
 
-if ($dbCheck) {
+if ($paramsCheck) {
+    $username   = $_POST[$params[0]];
+    $sessionkey = $_POST[$params[1]];
+    $rewardid   = $_POST[$params[2]];
 
-    // $check - authentication status
-    // $data - authentication failure message
-    include 'checkSessionkey.php';
+    // $db - database connection
+    // $dbCheck - database connection status
+    // $data - database connection failure message
+    include 'connectToDatabase.php';
 
-    if($check) {
+    if ($dbCheck) {
 
-        // Default error message
-        $data = array('response' => 'Server Error.');
+        // $check - authentication status
+        // $data - authentication failure message
+        include 'checkSessionkey.php';
 
-        // Get cost of reward
-        $stmt = $db->prepare('SELECT cost FROM rewards WHERE rewardid = ?');
-        $stmt->bind_param('i', $rewardid);
-        if ($stmt->execute()) {
-            $rewardCost = 0;
-            $stmt->bind_result($rewardCost);
-            $stmt->fetch();
-            $stmt->close();
+        if($check) {
 
-            // Get user points
-            $stmt = $db->prepare('SELECT points FROM users WHERE user_name = ?');
-            $stmt->bind_param('s', $username);
+            // Default error message
+            $data = array('response' => 'Server Error.');
+
+            // Get cost of reward
+            $stmt = $db->prepare('SELECT cost FROM rewards WHERE rewardid = ?');
+            $stmt->bind_param('i', $rewardid);
             if ($stmt->execute()) {
-                $userPoints = 0;
-                $stmt->bind_result($userPoints);
+                $rewardCost = 0;
+                $stmt->bind_result($rewardCost);
                 $stmt->fetch();
-                $stmt->close();            
+                $stmt->close();
 
-                if ($userPoints < $rewardCost) {
-                    $data = array('response' => 'Not enough points');
-                } else {
-                    $userPoints = $userPoints - $rewardCost;
-                    $stmt = $db->prepare('UPDATE users SET points = ? WHERE user_name = ?');
-                    $stmt->bind_param('is', $userPoints, $username);
-                    if ($stmt->execute()) {
-                        $data = array('response' => 'success');
+                // Get user points
+                $stmt = $db->prepare('SELECT points FROM users WHERE user_name = ?');
+                $stmt->bind_param('s', $username);
+                if ($stmt->execute()) {
+                    $userPoints = 0;
+                    $stmt->bind_result($userPoints);
+                    $stmt->fetch();
+                    $stmt->close();            
+
+                    if ($userPoints < $rewardCost) {
+                        $data = array('response' => 'Not enough points');
+                    } else {
+                        $userPoints = $userPoints - $rewardCost;
+                        $stmt = $db->prepare('UPDATE users SET points = ? WHERE user_name = ?');
+                        $stmt->bind_param('is', $userPoints, $username);
+                        if ($stmt->execute()) {
+                            $data = array('response' => 'success');
+                        }
                     }
                 }
             }
         }
-}}
+    }
+}
 echo json_encode($data);
 ?>
 
