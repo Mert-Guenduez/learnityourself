0cd207e9a0b593933f20b1e8adbb10c82ef1432f
Mert <mert.gunduz1997@gmail.com>
2019-06-11 10:17:03 +0200

    [Back][Feat] Added requested changes

--- Backend/getMissionTaskCompletion.php
+++ Backend/getMissionTaskCompletion.php
@@ -14,28 +14,22 @@ if ($paramsCheck) {
 
     include 'connectToDatabase.php';
     if ($dbCheck) {
-
         include 'checkSessionkey.php';
         if($check) {
             $stmt = $db->prepare('SELECT userToTask.username, COUNT(*)
                                     FROM tasks
                                     INNER JOIN userToTask
                                         ON tasks.id = userToTask.taskid
-                                    WHERE tasks.missionid = ? AND userToTask.completed  = 1
+                                    WHERE tasks.missionid = ? AND userToTask.completed = 1
                                     GROUP BY userToTask.username');
-    $stmt->bind_param('i', $missionid);
-    $stmt->execute();
-
-    $UserAmounts = $stmt->get_result();
-    $stmt->free_result();
-    $stmt->close();
-    $data = array();
-    $resultArray = array();
-    while($row = mysqli_fetch_array($UserAmounts)){
-    //$resultArray = array($row[0] => $row[1]);
-    $data[$row[0]] = $row[1] . "";
-
-}
-}}}
+            $stmt->bind_param('i', $missionid);
+            $stmt->execute();
+            $UserAmounts = $stmt->get_result();
+            $stmt->free_result();
+            $stmt->close();
+            $data = array();
+            while($row = mysqli_fetch_array($UserAmounts)){
+                $data[$row[0]] = $row[1] . "";
+}}}}
 echo json_encode($data);;
 ?>
