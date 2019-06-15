941618b40c4229eed24d48483b44153f8cf02682
Mert <mert.gunduz1997@gmail.com>
2019-06-15 10:18:42 +0200

    [Back][Feat] changed allAchievementsFromUser.php according to how Jonas wants it2

--- Backend/allAchievementsFromUser.php
+++ Backend/allAchievementsFromUser.php
@@ -31,7 +31,32 @@ if ($paramsCheck) {
 			$stmt->bind_param('s', $username);
 			if($stmt->execute()){
                 $row = mysqli_fetch_array($stmt->get_result());
-                $data = array('tasksCompleted' => $row[0], 'installedApp' => $row[1], 'totalEarnedPoints' => $row[2], 'totalRewardsAdded' => $row[3]);
+                echo $row[0] . $row[1] . $row[2] . $row[3];
+                $data = array();
+                if($row[0] >= 1){
+                    array_push($data, "Completed 1 Task");
+                }
+                if($row[0] >= 5){
+                    array_push($data, "Completed 5 Tasks");
+                }
+                if($row[0] >= 10){
+                    array_push($data, "Completed 10 Tasks");
+                }
+                if($row[1] == 1){
+                    array_push($data, "Installed the learnityourself App");
+                }
+                if($row[2] >= 10){
+                    array_push($data, "Earned 10 Points");
+                }
+                if($row[2] >= 50){
+                    array_push($data, "Earned 50 Points");
+                }
+                if($row[3] >= 1){
+                    array_push($data, "Added 1 Custom Reward");
+                }
+                if($row[3] >= 10){
+                    array_push($data, "Added 10 Custom Rewards");
+                }
             }
             else{
                 $data = array('error' => 'Could not get Achievements.');
